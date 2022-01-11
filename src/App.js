582a85521d58c3480e6f30bbe40d538d22e2c716
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-01-12 00:32:50 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Routes, Route, Switch } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Customers from './pages/Customers';
-
+// import { Routes, Route, Switch } from 'react-router-dom';
+// import Dashboard from './pages/Dashboard';
+// import Customers from './pages/Customers';
+import Sidebar from './components/layout/sidebar/Sidebar';
 
 function App() {
+
   return (
     <div className="App">
-      <Routes>
-        Route<Dashboard />
-        <Customers />
-
-      </Routes>
+      <Sidebar />
     </div >
   );
 }
---
+++ src/components/styles.jsx
@@ -0,0 +1,12 @@
+import { makeStyles } from '@mui/styles';
+
+
+export const useStyles = makeStyles({
+    activeClass: {
+        color: '#333'
+    },
+    ListItems: {
+        color: '#fff',
+        backgroundColor: '#FF7506'
+    }
+})
