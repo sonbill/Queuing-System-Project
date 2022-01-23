f50dd0c3a0500cde5f9c206f05774ab2c019887c
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-01-23 17:51:27 +0700

    update add equipment css

--- src/pages/add equipment/AddEquipment.jsx
+++ src/pages/add equipment/AddEquipment.jsx
@@ -9,47 +9,47 @@ function AddEquipment() {
   return (
     <div className="addEquipment">
       <TopNav />
-      <h1>Quản lý thiết bị</h1>
+      <h1 className="addEquipment__title">Quản lý thiết bị</h1>
       <div className="addEquipment__infor">
-        <h2 className="addEquipment__title">Thông tin thiết bị</h2>
+        <h2 className="addEquipment__infor__title">Thông tin thiết bị</h2>
         <div className="addEquipment__list">
           <div className="addEquipment__items">
             <div className="addEquipment__item">
-              <p>Mã thiết bị *</p>
+              <p>Mã thiết bị <span>*</span></p>
               <input type="text" placeholder="Nhập mã thiết bị" className="addEquipment__input" />
             </div>
             <div className="addEquipment__item">
-              <p>Loại thiết bị *</p>
-              <DropdownAddEquipment selected={selected} setSelected={setSelected} options={['Kiosk', 'Display counter']} />
+              <p>Loại thiết bị <span>*</span></p>
+              <DropdownAddEquipment selected={selected} setSelected={setSelected} options={['Kiosk', 'Display counter']} style={{ width: 770 }} />
             </div>
           </div>
           <div className="addEquipment__items">
             <div className="addEquipment__item">
-              <p>Tên thiết bị *</p>
+              <p>Tên thiết bị <span>*</span></p>
               <input type="text" placeholder="Nhập tên thiết bị" className="addEquipment__input" />
             </div>
             <div className="addEquipment__item">
-              <p>Tên đăng nhập *</p>
+              <p>Tên đăng nhập <span>*</span></p>
               <input type="text" placeholder="Nhập tài khoản" className="addEquipment__input" />
             </div>
           </div>
           <div className="addEquipment__items">
             <div className="addEquipment__item">
-              <p>Địa chỉ IP *</p>
+              <p>Địa chỉ IP <span>*</span></p>
               <input type="text" placeholder="Nhập địa chỉ IP" className="addEquipment__input" />
             </div>
             <div className="addEquipment__item">
-              <p>Mật khẩu *</p>
+              <p>Mật khẩu <span>*</span></p>
               <input type="password" placeholder="Nhập mật khẩu" className="addEquipment__input" />
             </div>
           </div>
           <div className="addEquipment__items">
-            <div className="addEquipment__item">
-              <p>Dịch vụ sử dụng *</p>
-              <input type="text" placeholder="Nhập mã thiết bị" className="addEquipment__input" />
+            <div className="addEquipment__item" style={{ flexGrow: 1 }}>
+              <p>Dịch vụ sử dụng <span>*</span></p>
+              <input type="text" placeholder="Nhập dịch vụ sử dụng" className="addEquipment__input" style={{ width: '99%' }} />
             </div>
           </div>
-          <p className="addEquipment__desc">* Là trường thông tin bắt buộc</p>
+          <p className="addEquipment__desc"><span>*</span> Là trường thông tin bắt buộc</p>
         </div>
       </div>
       <div className="addEquipment__buttons">
