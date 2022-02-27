48c72dd813d57b1efe6e9805931dea73690dcf15
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-02-27 22:56:23 +0700

    create new firebase-config

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDQvrRlWpsTqbZrmq7XNPi4Md6mbeJcWg",
+  authDomain: "queuing-system-project.firebaseapp.com",
+  projectId: "queuing-system-project",
+  storageBucket: "queuing-system-project.appspot.com",
+  messagingSenderId: "254362416071",
+  appId: "1:254362416071:web:9c02f39f1f2097cf945fe2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
