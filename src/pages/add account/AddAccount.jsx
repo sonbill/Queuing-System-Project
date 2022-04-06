f732d776a5286008e4993e869880f6536c320a90
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-04-06 16:27:03 +0700

    update push data to firestore add-account page

--- src/pages/add account/AddAccount.jsx
+++ src/pages/add account/AddAccount.jsx
@@ -4,6 +4,8 @@ import Dropdown from '../../components/Dropdown/Dropdown'
 import Sidebar from '../../components/sidebar_left/Sidebar'
 import { Link } from 'react-router-dom'
 
+import { db } from '../../firebase-config'
+import { collection, addDoc } from 'firebase/firestore'
 
 import './addAccount.css'
 
@@ -11,12 +13,36 @@ export default function AddAccount() {
   const [role, setRole] = useState('Chọn vai trò');
   const [status, setStatus] = useState('Hoạt động');
 
+  const [newFullname, setNewFullname] = useState([]);
+  const [newUsername, setNewUsername] = useState([]);
+  const [newPhone, setNewPhone] = useState([]);
+  const [newEmail, setNewEmail] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const collectionRef = collection(db, "users");
+    const payload = {
+      activityStatus: '',
+      email: newEmail,
+      fullname: newFullname,
+      role: '',
+      username: newUsername,
+      phone: newPhone,
+    }
+    await addDoc(collectionRef, payload);
+    document.getElementById("accountFullname").value = "";
+    document.getElementById("accountUsername").value = "";
+    document.getElementById("accountPhone").value = "";
+    document.getElementById("accountEmail").value = "";
+  }
+
+
   return (
     <div className="addAccount">
       <Sidebar />
       <div className="addAccount__layout">
         <TopNav name={'Thêm tài khoản'} />
-        <div>
+        <form onSubmit={handleSubmit}>
           <h1 className="addAccount__title">Quản lý tài khoản</h1>
           <div className="addAccount__infor">
             <h2 className="addAccount__infor__title">Thông tin tài khoản</h2>
@@ -24,17 +50,17 @@ export default function AddAccount() {
               <div className="addAccount__items">
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Họ tên<span>*</span></p>
-                  <input type="text" placeholder="Nhập họ tên" className="addAccount__input" />
+                  <input type="text" placeholder="Nhập họ tên" className="addAccount__input" id="accountFullname" onChange={(e) => setNewFullname(e.target.value)} />
                 </div>
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Tên đăng nhập:<span>*</span></p>
-                  <input type="text" placeholder="Nhập tên đăng nhập" className="addAccount__input" />
+                  <input type="text" placeholder="Nhập tên đăng nhập" className="addAccount__input" id="accountUsername" onChange={(e) => setNewUsername(e.target.value)} />
                 </div>
               </div>
               <div className="addAccount__items">
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Số điện thoại <span>*</span></p>
-                  <input type="text" placeholder="Nhập số điện thoại" className="addAccount__input" />
+                  <input type="text" placeholder="Nhập số điện thoại" className="addAccount__input" id="accountPhone" onChange={(e) => setNewPhone(e.target.value)} />
                 </div>
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Mật khẩu <span>*</span></p>
@@ -44,7 +70,7 @@ export default function AddAccount() {
               <div className="addAccount__items">
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Email <span>*</span></p>
-                  <input type="text" placeholder="Nhập địa chỉ Email" className="addAccount__input" />
+                  <input type="text" placeholder="Nhập địa chỉ Email" className="addAccount__input" id="accountEmail" onChange={(e) => setNewEmail(e.target.value)} />
                 </div>
                 <div className="addAccount__item">
                   <p className="addAccount__item__title">Nhập lại mật khẩu <span>*</span></p>
@@ -68,9 +94,9 @@ export default function AddAccount() {
             <Link to="/manage-account">
               <button className="addAccount__button addAccount__button-cancel">Huỷ bỏ</button>
             </Link>
-            <button className="addAccount__button addAccount__button-add">Thêm</button>
+            <button className="addAccount__button addAccount__button-add" >Thêm</button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
